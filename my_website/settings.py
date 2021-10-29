de22e23bc635c1d0dba32e0609fa6e9c01108459
Willow <willowthedev@protonmail.com>
2021-10-28 22:44:43 -0500

    my_website

--- my_website/settings.py
+++ my_website/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #'my_blog.apps.MyBlogConfig',
+    'my_blog.apps.MyBlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
