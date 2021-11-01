cedf7b51f5e1baf237d526040cb5e323d865fab2
Willow <willowthedev@protonmail.com>
2021-11-01 08:14:17 -0500

    added tailwind for future projects

--- my_website/settings.py
+++ my_website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY') or get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'taggit',
     'mathfilters',
+    'tailwind',
 ]
 
 MIDDLEWARE = [
